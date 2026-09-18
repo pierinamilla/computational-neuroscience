4cdf8c27c7f227b16a2221a23e349a1f399194dd
Pierina <millav@usp.br>
2026-09-18 06:17:46 -0300

    update

--- lista3/bin/run.sh
+++ lista3/bin/run.sh
@@ -1,10 +1,18 @@
-# Compilar módulos (primero los que no dependen de nadie)
-gfortran -O3 -Wall -Jbin -c ../src/mod_cs_params.f90 -o ./mod_cs_params.o
-gfortran -O3 -Wall -Jbin -c ../src/mod_cs_gating.f90 -o ./mod_cs_gating.o
-gfortran -O3 -Wall -Jbin -c ../src/mod_cs_deriv.f90  -o ./mod_cs_deriv.o
-gfortran -O3 -Wall -Jbin -c ../src/mod_rk4.f90       -o ./mod_rk4.o
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
 
-# Compilar programas principales
-gfortran -O3 -Wall -Jbin -o ./q1a ../src/q1a_main.f90 ./mod_cs_params.o ./mod_cs_gating.o
-gfortran -O3 -Wall -Jbin -o ./q1b ../src/q1b_main.f90 bin/mod_cs_params.o ./mod_cs_gating.o \
-                                                ./mod_cs_deriv.o  ./mod_rk4.o
+FC="gfortran"
+FFLAGS="-03 -Wall - Jbin"
+
+# Compilar módulos (primero los que no dependen de nadie)
+$FC $FFLAGS -c src/mod_cs_params.f90 -o bin/mod_cs_params.o
+$FC $FFLAGS -c src/mod_cs_gating.f90 -o bin/mod_cs_gating.o
+$FC $FFLAGS -c src/mod_cs_deriv.f90 -o bin/mod_cs_deriv.o
+$FC $FFLAGS -c src/mod_rk4.f90 -o bin/mod_rk4.f90
+
+# Compilar programas
+$FC $FFLAGS
+
+
+$FC $FFLAGS -o bin/q1b /src/q1b_main.f90 bin/mod_cs_params.o ./mod_cs_gating.o ./mod_cs_deriv.o  ./mod_rk4.o
